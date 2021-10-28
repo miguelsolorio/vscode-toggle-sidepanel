ed1fdf8e774dc2e63c20a20c7b3953564350f84d
Miguel Solorio <miguel.solorio@microsoft.com>
2021-10-28 13:35:03 -0700

    Add action for side panel toggle

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let sidePanelToggle: vscode.StatusBarItem;
+
+export function activate() {
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "miguelsolorio" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('miguelsolorio.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from toggle-sidepanel!');
-	});
-
-	context.subscriptions.push(disposable);
+	sidePanelToggle = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	sidePanelToggle.command = 'workbench.action.toggleAuxiliaryBar';
+	sidePanelToggle.text = '$(window)';
+	sidePanelToggle.tooltip = 'Toggle Side Panel';
+	sidePanelToggle.show();
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
